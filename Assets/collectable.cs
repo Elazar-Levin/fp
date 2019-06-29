1fe05dd88f3ea0a356287dbed491a4724c322b2c
Lazi <1504371@students.wits.ac.za>
2019-06-29 23:52:30 +0200

    stugff

---
+++ Assets/collectable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectable : MonoBehaviour {
+
+	public int value;
+	public int rotatespeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate(Vector3.up*Time.deltaTime*rotatespeed);
+	}
+	
+	void OnTriggerEnter()
+	{
+		Destroy(gameObject);
+	}
+}
